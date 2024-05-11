2913ca4da933d27029d9f7498125a67d3b03fa3e
SRaimundo <samuel.r.pinto@ufv.br>
2024-05-11 00:05:29 -0300

    add run.sh and template

---
+++ Codigos/Extra/template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define optimize ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl "\n"
+
+#define all(x) (x).begin(),(x).end()
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+
+int main(){
+    optimize;
+
+
+    return 0;
+}
