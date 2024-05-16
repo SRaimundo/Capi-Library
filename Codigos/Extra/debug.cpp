fd1599ba759ea0b9408923fbd146ec83b0da1934
Dener <deneribeiro10@hotmail.com>
2024-05-16 11:30:43 -0300

    adicionando parametro de debug em run.sh

--- Codigos/Extra/debug.cpp
+++ Codigos/Extra/debug.cpp
@@ -1,5 +1,13 @@
+// Debug
+// 1. Precisa compilar com -DDEBUG
+
+// para pair
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << "," << p.second << ')'; }
+
+// para vector,set,...
 template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ","; return os << '}'; }
+
+// debug
 void debug_out(string s) { cerr << endl; }
 template<typename H, typename... T>
 void debug_out(string s, H h, T... t) {
@@ -11,5 +19,5 @@ void debug_out(string s, H h, T... t) {
 #ifdef DEBUG
 #define debug(...) cerr << "Line(" << __LINE__ << "): "; debug_out(#__VA_ARGS__, __VA_ARGS__)
 #else
-#define debug(...) 42
+#define debug(...)
 #endif
