35fa3d2d9f02f565cf1966e92b39ae2b47a1160d
SRaimundo <samuel.r.pinto@ufv.br>
2024-05-28 23:31:15 -0300

    Add dijkstra

---
+++ Codigos/Grafos/dijkstra.cpp
@@ -0,0 +1,26 @@
+// Dijkstra
+
+// Calcula a menor distancia de x para todos o vertices
+// d[i] = INF => nao existe caminho
+
+//O(v log(e))
+
+typedef pair<int,int> pii;
+vector<vector<pii>> g;
+
+vector<int> dijkstra(int x){
+    vector<int> d(g.size(), INF);
+    priority_queue<pii, vector<pii>, greater<pii>> fila;
+    d[x] = 0;
+    fila.push({0, x});
+    while(!fila.empty()){
+        auto [vdist, v] = fila.top();
+        fila.pop();
+        if(vdist > d[v]) continue;
+        for(auto[u, udist] : g[v]) if(d[u] > d[v] + udist){
+            d[u] = d[v] + udist;
+            fila.push({d[u], u});
+        }
+    }
+    return d;
+}
